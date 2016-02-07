de506307b7e4c1c775bc4cd9ef37329b08486609
Kim Rutherford <kmr44@cam.ac.uk>
2016-02-08 10:18:13 +1300

    Copy public and private CV versions

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ DISPLAY=localhost:10.0 make html pdf public-cv=false HTMLTOPDF=wkhtmltopdf &&
     rsync -avP dist/cv.pdf $HOME/git/kimrutherford.github.io/cv-full.pdf
 
 DISPLAY=localhost:10.0 make html pdf public-cv=true HTMLTOPDF=wkhtmltopdf &&
-    rsync -avP dist/cv.html $HOME/git/kimrutherford.github.io/index.html
+    rsync -avP dist/cv.html $HOME/git/kimrutherford.github.io/index.html &&
+    rsync -avP dist/cv.html mac:Documents/pandoc-moderncv/cv-public.html
