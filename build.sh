7b47a3b651ed34237cf384e8ea51e2d8b913b2b5
Kim Rutherford <kmr44@cam.ac.uk>
2017-02-23 12:53:06 +1300

    Fix build for new desktop

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
 cd ~/git/pandoc-moderncv/
-DISPLAY=localhost:10.0 make html pdf public-cv=false HTMLTOPDF=wkhtmltopdf &&
-    rsync -avP dist/ mac:Documents/pandoc-moderncv/ &&
-    rsync -avP dist/ oliver0:/var/www/pombase/kmr44/pandoc-moderncv/ &&
+make html pdf public-cv=false HTMLTOPDF=wkhtmltopdf &&
     rsync -avP dist/cv.html $HOME/git/kimrutherford.github.io/cv-full.html &&
     rsync -avP dist/cv.pdf $HOME/git/kimrutherford.github.io/cv-full.pdf
 
-DISPLAY=localhost:10.0 make html pdf public-cv=true HTMLTOPDF=wkhtmltopdf &&
-    rsync -avP dist/cv.html $HOME/git/kimrutherford.github.io/index.html &&
-    rsync -avP dist/cv.html mac:Documents/pandoc-moderncv/cv-public.html
+make html pdf public-cv=true HTMLTOPDF=wkhtmltopdf &&
+    rsync -avP dist/cv.html $HOME/git/kimrutherford.github.io/index.html
